604db65e966b1c4cdc9abaae5bc1fb1ba0f89556
Buichl <212568043@logon.ds.ge.com>
2019-09-09 09:50:45 -0500

    Added ship enum and fixed onHover

--- scripts/game.js
+++ scripts/game.js
@@ -7,7 +7,7 @@ var spaceLanes = [];
 var fleets;
 var planetNames = ["Argys", "Bonton", "Crysis", "Dingus", "Elmo", "FoxDude", "GizmoWorld", "Hai", "Islendar", "Joko", "Klamata", "Lister"];
 var maxNumberPlanets = planetNames.length;
-var planetTechLevelDisto = [1,1,2,2,3,3,4,4,5,5,6,6];
+var planetTechLevelDisto = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 var maxX = 600;
 var maxY = 600;
 var mapBorder = 20;
@@ -30,25 +30,24 @@ function init() {
 	//Make the canvas and get the context
 	canvas = document.getElementById("myCanvas");
 	ctx = canvas.getContext("2d");
-	
+
 	//Set the intial game state
 	currentPlayer = 'Red';
-	
+
 	//create an array of random planets
 	for (i = 0; i < maxNumberPlanets; i++) {
 		planets = createRandomPlanets();
 	}
-	
+
 	//set up the mouse listener for selecting planets
 	canvas.onmousedown = mouseListener;
-	
+	canvas.onmousemove = mouseListener;
+
 	//Make spacelanes between the planets
-	for (var i = 0; i < maxNumberPlanets; i++)
-	{
-		for (var j = 0; j < maxNumberPlanets; j++){
-			if(i != j )
-			{
-				spaceLanes.push(new SpaceLane(planets[i], planets[j], calculateSpaceLaneDistance(planets[i], planets[j]) ));
+	for (var i = 0; i < maxNumberPlanets; i++) {
+		for (var j = 0; j < maxNumberPlanets; j++) {
+			if (i != j) {
+				spaceLanes.push(new SpaceLane(planets[i], planets[j], calculateSpaceLaneDistance(planets[i], planets[j])));
 			}
 		}
 	}
@@ -59,7 +58,7 @@ function init() {
 
 	//make the player objects
 	players = [new Player(player1Id), new Player(player2Id)];
-		
+
 	drawMap();
 
 	//Initialize buttons
@@ -67,14 +66,18 @@ function init() {
 	//document.getElementsByName("sendShips")[0].disabled = true;
 	document.getElementsByName("upgradeTech")[0].disabled = true;
 	document.getElementsByName("decreaseTech")[0].disabled = true;
-	
+
 }
 
-function calculateSpaceLaneDistance(planetFrom, planetTo){
+function testFunc() {
+	console.log("test");
+}
+
+function calculateSpaceLaneDistance(planetFrom, planetTo) {
 	//We're going to split the max path distance into 3 parts distance 1,2,3
 	const distanceThresholds = [longestPossiblePath / 4, longestPossiblePath / 2];
 	const d = planetDistance(planetFrom, planetTo);
-	if(d < distanceThresholds[0])
+	if (d < distanceThresholds[0])
 		return 1;
 	else if (d < distanceThresholds[1])
 		return 2;
@@ -84,27 +87,27 @@ function calculateSpaceLaneDistance(planetFrom, planetTo){
 }
 
 function mouseListener(e) {
-    // Get the current mouse position
-    var r = canvas.getBoundingClientRect(),
-        x = e.clientX - r.left, y = e.clientY - r.top;
-    var clickedPlanet = false;
+	// Get the current mouse position
+	var r = canvas.getBoundingClientRect(),
+		x = e.clientX - r.left, y = e.clientY - r.top;
+	var clickedPlanet = false;
 
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+	//ctx.clearRect(0, 0, canvas.width, canvas.height);
+	selectedPlanetIndex = -1;
 
-    for(var i = planets.length - 1, b; b = planets[i]; i--) {
-        if(x >= b.x - planetDiameter && x <= b.x + planetDiameter * 2 &&
-           y >= b.y - planetDiameter && y <= b.y + planetDiameter * 2) {
-            // The mouse honestly hits the rect
-            clickedPlanet = true;
-            selectedPlanetIndex = i;
-            break;
-        }
-    }
-	
-    // Draw the rectangles by Z (ASC)
-    if (clickedPlanet || !clickedPlanet && selectedPlanetIndex )
-	{
-		if(!clickedPlanet && selectedPlanetIndex)
+	for (var i = planets.length - 1, b; b = planets[i]; i--) {
+		if (x >= b.x - planetDiameter && x <= b.x + planetDiameter * 2 &&
+			y >= b.y - planetDiameter && y <= b.y + planetDiameter * 2) {
+			// The mouse honestly hits the rect
+			clickedPlanet = true;
+			selectedPlanetIndex = i;
+			break;
+		}
+	}
+
+	// Draw the rectangles by Z (ASC)
+	if (clickedPlanet || !clickedPlanet && selectedPlanetIndex != -1) {
+		if (!clickedPlanet && selectedPlanetIndex)
 			selectedPlanetIndex = null;
 		drawMap();
 		refreshSelectedPlanetInfo(planets[selectedPlanetIndex]);
@@ -112,24 +115,24 @@ function mouseListener(e) {
 }
 
 function refreshSelectedPlanetInfo(planet) {
-	var planetsIown = planets.filter((p) => p.owner === currentPlayer).map((p)=> {return p.id;});
-	var planetsWhereIHaveFleets = fleets.filter((f) => f.owner === currentPlayer && !f.remainingTransit).map((f) => {return f.location});
-	
+	var planetsIown = planets.filter((p) => p.owner === currentPlayer).map((p) => { return p.id; });
+	var planetsWhereIHaveFleets = fleets.filter((f) => f.owner === currentPlayer && !f.remainingTransit).map((f) => { return f.location });
+
 	document.getElementsByName("planetName")[0].value = planet.name;
 
-	if(planetsIown.includes(planet.id) || planetsWhereIHaveFleets.includes(planet.id)) {
+	if (planetsIown.includes(planet.id) || planetsWhereIHaveFleets.includes(planet.id)) {
 		document.getElementsByName("techLevel")[0].value = planet.techLevel;
 		document.getElementsByName("ownerName")[0].value = planet.owner;
 		document.getElementsByName("incomePerTurn")[0].value = planet.income;
-		document.getElementsByName("shipsRed")[0].value = 
-			fleetToString(fleets.filter((f) => f.owner === 'Red' && f.location === planet.id && f.remainingTransit < 1 )[0]);
-		document.getElementsByName("shipsBlue")[0].value = 
-			fleetToString(fleets.filter((f) => f.owner === 'Blue' && f.location === planet.id && f.remainingTransit < 1 )[0]);
-		document.getElementsByName("shipsNative")[0].value = 
-			fleetToString(fleets.filter((f) => f.owner === 'Native' && f.location === planet.id && f.remainingTransit < 1 )[0]);
+		document.getElementsByName("shipsRed")[0].value =
+			fleetToString(fleets.filter((f) => f.owner === 'Red' && f.location === planet.id && f.remainingTransit < 1)[0]);
+		document.getElementsByName("shipsBlue")[0].value =
+			fleetToString(fleets.filter((f) => f.owner === 'Blue' && f.location === planet.id && f.remainingTransit < 1)[0]);
+		document.getElementsByName("shipsNative")[0].value =
+			fleetToString(fleets.filter((f) => f.owner === 'Native' && f.location === planet.id && f.remainingTransit < 1)[0]);
 
 		var techLevelsAllowBuilding = shipTypes.map((x) => x.techLevel);
-		if(selectedPlanetIndex && techLevelsAllowBuilding.includes(planets[selectedPlanetIndex].techLevel)){
+		if (selectedPlanetIndex && techLevelsAllowBuilding.includes(planets[selectedPlanetIndex].techLevel)) {
 			document.getElementsByName("buildShip")[0].disabled = false;
 		}
 		else {
@@ -151,18 +154,18 @@ function refreshSelectedPlanetInfo(planet) {
 function fleetToString(fleet) {
 	if (!fleet)
 		return "none";
-	return "F:"+fleet.ships.fighter+",Sb:"+fleet.ships.shotgunBoat;
+	return "F:" + fleet.ships.fighter + ",Sb:" + fleet.ships.shotgunBoat;
 }
 
-function drawMap(){
+function drawMap() {
 	//clear the map by drawing the background color over the whole thing
 	ctx.beginPath();
 	ctx.rect(0, 0, maxX, maxY);
 	ctx.fillStyle = "#eee";
 	ctx.fill();
 	ctx.closePath();
-	
-	if(selectedPlanetIndex != null) {
+
+	if (selectedPlanetIndex != null) {
 		drawSpaceLanes();
 	}
 
@@ -176,7 +179,7 @@ function drawMap(){
 	for (var i = 0; i < playerFleets.length; i++) {
 		var planet = planets.filter((p) => p.id === playerFleets[i].location)[0];
 		drawFleet(playerFleets[i], planet);
-		
+
 		var enemyFleets = fleets.filter((f) => f.location === playerFleets[i].location && !f.remainingTransit);
 
 		enemyFleets.forEach((ef) => drawFleet(ef, planet));
@@ -184,8 +187,7 @@ function drawMap(){
 
 }
 
-function drawSpaceLanes()
-{
+function drawSpaceLanes() {
 	var spaceLanesToShow = spaceLanes.filter((sp) => sp.planetTo.id === selectedPlanetIndex);
 
 	spaceLanesToShow.forEach((sp) => {
@@ -206,7 +208,7 @@ function drawSpaceLanes()
 
 }
 
-function drawFleet(fleet, planet){
+function drawFleet(fleet, planet) {
 	var dx;
 	var dy;
 	ctx.beginPath();
@@ -215,19 +217,19 @@ function drawFleet(fleet, planet){
 			ctx.fillStyle = "red";
 			dx = -4;
 			dy = -4;
-		break;
+			break;
 		case 'Blue':
 			ctx.fillStyle = "blue";
 			dx = 4;
 			dy = -4;
-		break;
+			break;
 		default:
 			ctx.fillStyle = "white";
 			dx = 0;
 			dy = -2;
-		break;
+			break;
 	}
-	ctx.arc(planet.x + dx, planet.y + dy, 2, 0, Math.PI*2, false);
+	ctx.arc(planet.x + dx, planet.y + dy, 2, 0, Math.PI * 2, false);
 	ctx.fill();
 	ctx.closePath();
 
@@ -235,22 +237,21 @@ function drawFleet(fleet, planet){
 
 function drawPlanet(planet, selected) {
 	ctx.beginPath();
-	ctx.arc(planet.x, planet.y, planetDiameter/2, 0, Math.PI*2, false);
+	ctx.arc(planet.x, planet.y, planetDiameter / 2, 0, Math.PI * 2, false);
 	if (selected)
 		ctx.fillStyle = "green";
 	else
 		ctx.fillStyle = "gray";
 	ctx.fill();
 	ctx.closePath();
-	
+
 	ctx.fillStyle = "black";
 	ctx.textAlign = "center";
 	ctx.font = "12px Arial";
 	ctx.fillText(planet.name, planet.x, planet.y - 15);
 }
 
-function createRandomPlanets(name)
-{
+function createRandomPlanets(name) {
 	var planets = [];
 	shuffle(planetTechLevelDisto);
 	for (i = 0; i < maxNumberPlanets; i++) {
@@ -262,8 +263,8 @@ function createRandomPlanets(name)
 		planet.techLevel = planetTechLevelDisto[i];
 		planet.income = 100 * planet.techLevel;
 		planet.owner = 'Native';
-		if(planet.techLevel > 4) {
-			var ships = new Ships(getRndInteger(1,planet.techLevel), 0);
+		if (planet.techLevel > 4) {
+			var ships = new Ships(getRndInteger(1, planet.techLevel), 0);
 			var fleet = new Fleet('Native', i, 0, ships);
 			fleets.push(fleet);
 		}
@@ -273,8 +274,8 @@ function createRandomPlanets(name)
 
 	var listOfTooClosePlanets = [];
 	do {
-		if(listOfTooClosePlanets.length > 0) {
-			for(var k = 0; k < listOfTooClosePlanets.length; k++){
+		if (listOfTooClosePlanets.length > 0) {
+			for (var k = 0; k < listOfTooClosePlanets.length; k++) {
 				planets[listOfTooClosePlanets[k]].x = (getRndInteger(mapBorder, maxX - mapBorder));
 				planets[listOfTooClosePlanets[k]].y = (getRndInteger(mapBorder, maxY - mapBorder));
 			}
@@ -284,38 +285,38 @@ function createRandomPlanets(name)
 		//reposition planets to be not to close together
 		for (var i = 0; i < maxNumberPlanets; i++) {
 			for (var j = 0; j < maxNumberPlanets; j++) {
-				if(i != j && !listOfTooClosePlanets.includes(i) && planetDistance(planets[i], planets[j]) < planetDiameter * 2) {
+				if (i != j && !listOfTooClosePlanets.includes(i) && planetDistance(planets[i], planets[j]) < planetDiameter * 2) {
 					listOfTooClosePlanets.push(i);
 				}
 			}
 		}
 	}
-	while(listOfTooClosePlanets.length > 0);
+	while (listOfTooClosePlanets.length > 0);
 
 	return planets;
 }
 
-function planetDistance (planeta, planetb){
+function planetDistance(planeta, planetb) {
 	return distance(planeta.x, planeta.y, planetb.x, planetb.y);
 }
 
 function createRandomHomePlanet(playerId) {
 
 	var index = getRndInteger(0, maxNumberPlanets - 1);
-	while(planets[index].owner != 'Native'){
+	while (planets[index].owner != 'Native') {
 		index = (index + 1) % (maxNumberPlanets - 1);
 	}
 
 	planets[index].owner = playerId;
 	planets[index].techLevel = 7;
 	planets[index].income = 100 * planets[index].techLevel;
-	var ships = new Ships(10,0);
+	var ships = new Ships(10, 0);
 	var fleet = new Fleet(playerId, index, 0, ships);
 	fleets.push(fleet);
 
 	//remove Native fleets from home planet
 	fleets = fleets.filter((f) => f.location != index || f.owner != 'Native');
-	
+
 }
 
 
@@ -324,7 +325,7 @@ function processBattle(fleet1, fleet2) {
 }
 
 function advanceOneTurn() {
-	if(currentTurn !=0) {
+	if (currentTurn != 0) {
 		currentPlayer = currentPlayer == player1Id ? player2Id : player1Id;
 	} else {
 		document.getElementsByName("startGameEndTurn")[0].value = 'End Turn';
@@ -332,11 +333,11 @@ function advanceOneTurn() {
 
 	var currentPlayerIndex = players.findIndex((p) => p.id === currentPlayer);
 
-	currentTurn +=1;
+	currentTurn += 1;
 
 	//Give the player their income
-	var creditsEarned = planets.filter((p) => p.owner === currentPlayer).map((p) => p.income).reduce((total,planet) => total + planet.income);
-	
+	var creditsEarned = planets.filter((p) => p.owner === currentPlayer).map((p) => p.income).reduce((total, planet) => total + planet.income);
+
 	players[currentPlayerIndex].credits += creditsEarned;
 
 	//Resolve battles
@@ -346,7 +347,7 @@ function advanceOneTurn() {
 	//Process Native production and rebellion
 
 	//Check for victory or loss
-	
+
 
 	//Update the display
 	document.getElementsByName("credits")[0].value = players[currentPlayerIndex].credits;
--- scripts/models.js
+++ scripts/models.js
@@ -1,42 +1,35 @@
-function Fleet(owner, location, remainingTransit, ships)
-{
-	this.owner = owner;
-	this.location = location;
-	this.remainingTransit = remainingTransit;
-	this.ships = ships;
+function Fleet(owner, location, remainingTransit, ships) {
+    this.owner = owner;
+    this.location = location;
+    this.remainingTransit = remainingTransit;
+    this.ships = ships;
 }
 
-function Ships(fighter, shotgunBoat){
-	this.fighter = fighter;
-	this.shotgunBoat = shotgunBoat;
+function Ships(fighter, shotgunBoat) {
+    this.fighter = fighter;
+    this.shotgunBoat = shotgunBoat;
 }
 
-function Player(id){
+function Player(id) {
     this.id = id;
     this.credits = 0;
 
 }
 
-// function ShipType(name, attack, hp, phase, durability, cost, techLevel, specialAttack) {
-//     this.name = name;
-//     this.attack = attack;
-//     this.hp = hp;
-//     this.phase = phase;
-//     this.durability = durability;
-//     this.cost = cost;
-//     this.techLevel = techLevel;
-//     this.specialAttack = specialAttack;
-// }
-
-function SpaceLane(planetFrom, planetTo, transitTime)
-{
+function SpaceLane(planetFrom, planetTo, transitTime) {
     this.planetFrom = planetFrom;
     this.planetTo = planetTo;
     this.transitTime = transitTime;
 }
 
-class Ship{
-    constructor(count, name, attack, hp, phase, durability, cost, techLevel, specialAttack){
+// ShipType Enum
+const ShipType = {
+    Fighter: "Fighter",
+    ShotgunBoat: "ShotgunBoat"
+};
+
+class Ship {
+    constructor(count, name, attack, hp, phase, durability, cost, techLevel, specialAttack) {
         this.count = count;
         this.name = name;
         this.attack = attack;
@@ -49,14 +42,14 @@ class Ship{
     }
 }
 
-class FighterShip extends Ship{
+class FighterShip extends Ship {
     constructor(count) {
-        super(count, "Fighter", 7, 5, 3, 5, 25, 7, 0);
+        super(count, ShipType.Fighter, 7, 5, 3, 5, 25, 7, 0);
     }
 }
 
-class ShotGunBoatShip extends Ship{
+class ShotGunBoatShip extends Ship {
     constructor(count) {
-        super(count, "ShotgunBoat", 10, 3, 2, 5, 50, 7, 0);
+        super(count, ShipType.ShotGunBoatShip, 10, 3, 2, 5, 50, 7, 0);
     }
 }
